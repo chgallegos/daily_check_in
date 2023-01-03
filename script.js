7506dc0a24ae2d6c7b0fd8804a140800b7359d7b
ChrisG <chrisale89@gmail.com>
2023-01-02 23:20:39 -0700

    finalizing

--- script.js
+++ script.js
@@ -100,18 +100,14 @@ Plotly.newPlot('Physical', physicalData, layout);
         // to screenshot the div
         function takeshot() {
           let div =
-              document.getElementById('screenshot');
+            document.getElementById('screenshot'); 
 
-          // Use the html2canvas
-          // function to take a screenshot
-          // and append it
-          // to the output div
           html2canvas(div).then(
               function (canvas) {
                   document
                   .getElementById('output')
                   .appendChild(canvas);
               })
+              
       }
-
-  
+      
